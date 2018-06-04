cf81688d458ec098e383d296baaf52d22551dd87
Katerina Roukounaki <k4t3r1n4@gmail.com>
2018-06-04 23:12:50 +0300

    Check model repository health

--- model-repository/routes/index.js
+++ model-repository/routes/index.js
@@ -4,6 +4,7 @@ const analyticsProcessorDefinitions = require('./analytics-processor-definitions
 const dataInterfaces = require('./data-interfaces');
 const dataKinds = require('./data-kinds');
 const dataSourceDefinitions = require('./data-source-definitions');
+const healthCheck = require('./health-check');
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/analytics-processor-definitions', analyticsProcessorDefinitions);
 router.use('/data-interfaces', dataInterfaces);
 router.use('/data-kinds', dataKinds);
 router.use('/data-source-definitions', dataSourceDefinitions);
+router.use('/health-check', healthCheck);
 
 module.exports = router;
